ea9382aea7889ce6fb199c2782a014d8dd0d205a
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2020-05-21 11:50:15 +0300

    Initial Commit

---
+++ Speedometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speedometer
+//
+//  Created by Berkant Beğdilili on 21.05.2020.
+//  Copyright © 2020 Berkant Beğdilili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
